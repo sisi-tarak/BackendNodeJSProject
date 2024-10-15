86d4eebd7e3e0cdc4057b683c4dfbabc4b72e718
Sisindri Singamsetti <sisindrisingamsetti@gmail.com>
2024-10-15 21:59:51 +0530

    created login credentials and text after submit the button

--- Server.js
+++ Server.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/sisi", (req, res) => {
   res.render("homepage");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("Thank your for filling the form");
 });
 
 app.listen(3000);
