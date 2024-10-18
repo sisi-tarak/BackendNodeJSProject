0a03f98b3febfe0435e0af76bf76afc09add3e56
Sisindri Singamsetti <sisindrisingamsetti@gmail.com>
2024-10-18 17:44:22 +0530

    validation for username only

--- Server.js
+++ Server.js
@@ -5,17 +5,46 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-app.get("/sisi", (req, res) => {
-  res.render("homepage");
+app.use(function (req, res, next) {
+  res.locals.errors = [];
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("homepage", { errors: [] });
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { errors: [] });
 });
 
 app.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("Thank your for filling the form");
+  const errors = [];
+
+  if (typeof req.body.username !== "string") req.body.username = "";
+  if (typeof req.body.password !== "string") req.body.password = "";
+
+  const username = req.body.username.trim();
+
+  if (!username) {
+    errors.push("You must provide Username");
+  } else {
+    if (username.length < 8) {
+      errors.push("Username must be at least 8 characters");
+    }
+    if (username.length > 12) {
+      errors.push("Username cannot exceed 12 characters");
+    }
+    if (!username.match(/^[a-zA-Z0-9]+$/)) {
+      errors.push("Username can only contain letters and numbers");
+    }
+  }
+
+  if (errors.length > 0) {
+    res.render("homepage", { errors });
+  } else {
+    res.send("Thank you for filling the form");
+  }
 });
 
 app.listen(3000);
